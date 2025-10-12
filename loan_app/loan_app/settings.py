0820e1876d9178a63db7e01ecfdb6df344d1e4a8
cybercodemomo <cybercodemomo@gmail.com>
2025-10-12 16:11:25 +0100

    Fix:Updates

--- loan_app/loan_app/settings.py
+++ loan_app/loan_app/settings.py
@@ -167,7 +167,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # SMTP configuration for Gmail
-EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
